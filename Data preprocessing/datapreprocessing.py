a6df948bdcdfb27d4a168a74b6ab5f39cfa3cb43
Aman Kumar Jain <jainamandelhi@gmail.com>
2019-02-02 17:12:35 +0530

    Update datapreprocessing.py

--- Data preprocessing/datapreprocessing.py
+++ Data preprocessing/datapreprocessing.py
@@ -34,10 +34,20 @@ labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
 
-# Feature Scaling
+# Feature Scaling(Standard Scaling)
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)
+
+
+#Min_Max_Scaling
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+from sklearn.linear_model import Ridge
+X_train, X_test, y_train, y_test = train_test_split(X_crime, y_crime,
+                                                   random_state = 0)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
